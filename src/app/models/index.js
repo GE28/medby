c477a2303fdf2c318857dc62b133babf09b1f54b
GE28 <44209041+GE28@users.noreply.github.com>
2020-11-07 20:57:40 -0300

    Add comments to index.js

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -5,17 +5,22 @@ import Sequelize from 'sequelize';
 
 import config from '../../config/sequelize';
 
+// This file's name
 const basename = path.basename(__filename);
 
 const models = [];
 
+// file's extension = "js"
+// file ≠ this file
 function isValidModel(file) {
   return file.slice(-3) === '.js' && file !== basename;
 }
 
+// Get all the .js files in this directory
 fs.readdirSync(__dirname)
   .filter((file) => isValidModel(file))
   .forEach(async (file) => {
+    // Import file
     const model = require(path.join(__dirname, file));
     models.push(model);
   });
@@ -24,6 +29,7 @@ class Connection {
   constructor() {
     this.sequelize = new Sequelize(config);
 
+    // Importing models into sequelize
     models
       .map((Model) => Model.default.init(this.sequelize))
       .map(
