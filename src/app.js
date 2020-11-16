9a007b03a6c7d5cfb9a060ca0a3088be81de0e4f
GE28 <44209041+GE28@users.noreply.github.com>
2020-11-16 02:10:34 -0300

    Update AvailableTimes each 15 minutes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import Express, { json } from 'express';
 
+import Bull from 'bull';
+
 import './app/models'; // starting Sequelize
 import './app/models/mongo'; // starting Mongoose
 
 import routes from './routes';
 import jsonValidator from './middlewares/jsonValidator';
+
+import availabilityJob from './app/jobs/AvailabilityUpdate';
+import { options, redis } from './config/bull';
 
 class App {
   constructor() {
@@ -17,6 +22,18 @@ class App {
   middlewares() {
     this.app.use(json());
     this.app.use(jsonValidator);
+  }
+
+  queue() {
+    this.queue = new Bull('availability', ...redis);
+
+    this.queue.process('updateTimes', availabilityJob);
+
+    this.queue.empty();
+
+    this.queue.add('updateTimes', options, {
+      repeat: { cron: '0,15,30,45 * * * *' },
+    });
   }
 }
 
---
+++ src/config/bull.js
@@ -0,0 +1,9 @@
+export default {
+  options: {
+    allowedFutureWeeks: 3,
+    minPastHours: 2,
+  },
+  redis: {
+    url: 'redis://127.0.0.1:6379',
+  },
+};
