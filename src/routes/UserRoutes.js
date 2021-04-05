a50d2f02b2d126ca90b6eeda49f08604f92b5e11
GE28 <44209041+GE28@users.noreply.github.com>
2021-04-05 20:31:43 -0300

    change routes code style

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -10,7 +10,6 @@ import tokenValidator from '../middlewares/tokenValidator';
 const userRoutes = new Router();
 
 userRoutes.post('/signup', UserController.store);
-
 userRoutes.post('/login', TokenController.store);
 
 const tokenVal = tokenValidator('U');
@@ -20,9 +19,7 @@ userRoutes.get('/profile', tokenVal, UserController.show);
 const tokenIdVal = [tokenVal, idValidator];
 
 userRoutes.get('/appointments/:id', tokenIdVal, AppointmentController.show);
-
 userRoutes.get('/appointments/:id', tokenIdVal, AppointmentController.update);
-
 userRoutes.post('/appointments/available/', tokenVal, ATimeController.index);
 
 export default userRoutes;
