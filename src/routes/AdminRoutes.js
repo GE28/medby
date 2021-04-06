330ec9f77bb88d5f6dab9ab0f62167982d1122d8
GE28 <44209041+GE28@users.noreply.github.com>
2021-04-05 21:32:04 -0300

    chande admin routes code style

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -16,41 +16,27 @@ const tokenVal = tokenValidator('A');
 const tokenIdVal = [tokenVal, idValidator];
 
 adminRoutes.get('/users/list', tokenVal, UserController.index);
-
 adminRoutes.get('/users/:id', tokenIdVal, UserController.show);
 
 adminRoutes.get('/doctors/list', tokenVal, DoctorController.index);
-
 adminRoutes.get('/doctors/:id', tokenIdVal, DoctorController.show);
-
 adminRoutes.post('/doctors/', tokenVal, DoctorController.store);
-
 adminRoutes.delete('/doctors/:id', tokenIdVal, DoctorController.delete);
 
 adminRoutes.get('/units/list', tokenVal, UnitController.index);
-
 adminRoutes.get('/units/:id', tokenIdVal, UnitController.show);
-
 adminRoutes.get('/units/:id', tokenIdVal, UnitController.update);
-
 adminRoutes.post('/units/', tokenVal, UnitController.store);
-
 adminRoutes.delete('/units/:id', tokenIdVal, UnitController.delete);
 
 adminRoutes.get('/specialties/list', tokenVal, SpecialtyController.index);
-
 adminRoutes.get('/specialties/:id', tokenIdVal, SpecialtyController.show);
-
 adminRoutes.get('/specialties/:id', tokenIdVal, SpecialtyController.update);
-
 adminRoutes.post('/specialties/', tokenVal, SpecialtyController.store);
 
 adminRoutes.delete('/timetable/:id', tokenIdVal, TimetableController.delete);
-
 adminRoutes.get('/timetable/list', tokenVal, TimetableController.index);
-
 adminRoutes.post('/timetable/', tokenVal, TimetableController.store);
-
 adminRoutes.delete('/timetable/:id', tokenIdVal, TimetableController.delete);
 
 adminRoutes.get('/appointments/list', tokenVal, SpecialtyController.index);
