a2854ff15067e9f05ba0f0b17ecdcd76042968f5
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-16 21:10:08 -0300

    Add appointment preview method

--- src/app/controllers/AvailableTimeController.js
+++ src/app/controllers/AvailableTimeController.js
@@ -1,50 +1,123 @@
-import { addHours } from 'date-fns';
+import { addHours, isBefore } from 'date-fns';
 import Joi from '@hapi/joi';
+import { isValidObjectId } from 'mongoose';
 
 import AvailableTime from '../models/mongo/AvailableTime';
+import Doctor from '../models/Doctor';
+import Specialty from '../models/Specialty';
+import Unit from '../models/Unit';
+
+const getFutureDays = () => {
+  const futureWeeks = Number(process.env.FUTURE_WEEKS) || 3;
+  return futureWeeks * 7;
+};
 
 class AvailableTimeController {
   async index(req, res) {
-    const minPastHours = process.env.PAST_HOURS;
+    const minPastHours = Number(process.env.PAST_HOURS);
 
-    const querySchema = Joi.object({
-      min_date: Joi.date(),
-      max_date: Joi.date(),
+    const metadata = { maxAllowedDaysInFuture: getFutureDays(), count: null };
+
+    const { config_only } = req.query;
+    if (config_only > 0) return res.json({ metadata });
+
+    const filters = Joi.object({
+      min_date: Joi.date().iso(),
+      max_date: Joi.date().iso(),
       page: Joi.number().integer().min(1).default(1),
-    });
-
-    const bodySChema = Joi.object({
       doctor_id: Joi.string().uuid(),
       unit_id: Joi.string().uuid(),
-      spec_id: Joi.string().uuid(),
-    });
+      spec_id: Joi.string().uuid().required(),
+    }).unknown(true);
 
-    let query, body;
+    let query;
 
     try {
-      query = await querySchema.validateAsync(req.query);
-      body = await bodySChema.validateAsync(req.body);
+      query = await filters.validateAsync(req.query);
     } catch (err) {
-      return res
-        .status(400)
-        .json({ error: 'Bad request', details: { ...err } });
+      return res.status(400).json({ error: 'Bad request', ...{ err } });
     }
 
-    const { page, max_date } = query;
-    const min_date = query.min_date || addHours(new Date(), minPastHours);
+    const { max_date, page } = query;
+    delete query.max_date;
+    delete query.page;
 
-    const availableTimeList = await AvailableTime.find({
-      ...body,
+    const minAllowedDate = addHours(new Date(), minPastHours);
+    const min_date = isBefore(minAllowedDate, query.min_date)
+      ? query.min_date
+      : minAllowedDate;
+    delete query.min_date;
+
+    const options = {
+      ...query,
       taken: false,
       date: {
         $gte: min_date,
         ...(max_date && { $lt: max_date }),
       },
-    })
+    };
+
+    const availableTimeList = await AvailableTime.find(options)
       .skip(page * 10 - 10)
       .limit(10);
 
-    return res.json(availableTimeList);
+    metadata.count = await AvailableTime.countDocuments(options);
+
+    return res.json({
+      data: [...availableTimeList],
+      metadata,
+    });
+  }
+
+  async show(req, res) {
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+      return res.status(400).json({ error: 'Bad request' });
+    }
+
+    const availableTime = await AvailableTime.findById(id);
+
+    if (!availableTime) {
+      return res
+        .status(403)
+        .json({ error: 'Specified register was not found' });
+    }
+
+    const { doctor_id, date: time } = availableTime;
+
+    try {
+      const doctor = await Doctor.findByPk(doctor_id, {
+        include: [
+          {
+            model: Unit,
+            as: 'unit',
+            attributes: {
+              exclude: ['created_at', 'updated_at'],
+            },
+          },
+          {
+            model: Specialty,
+            as: 'specialty',
+            attributes: {
+              exclude: ['created_at', 'updated_at'],
+            },
+          },
+        ],
+        exclude: ['created_at', 'updated_at'],
+        raw: true,
+        nest: true,
+      });
+
+      return res.json({
+        aT_id: id,
+        aT_date: time,
+        final_price: doctor.specialty.base_price,
+        doctor,
+      });
+    } catch (err) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
   }
 }
 
