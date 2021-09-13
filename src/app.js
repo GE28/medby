bac4475c03365244eed19c9ec226a1408325292d
GE28 <44209041+GE28@users.noreply.github.com>
2021-09-13 16:45:25 -0300

    Allow any origin requests

--- src/app.js
+++ src/app.js
@@ -23,7 +23,6 @@ class App {
     this.app.use(helmet());
     this.app.use(
       cors({
-        origin: ['http://localhost:3000'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
       })
